9c0534443961e9934469931c7d60b45334fca6b3
Đoàn Thế Khiêm <doanthekhiem@gmail.com>
2024-03-11 17:38:32 +0700

    connect

--- pages/app/Layout.js
+++ pages/app/Layout.js
@@ -1,39 +1,83 @@
 import React from "react";
 import ButtonPrimary from "../../components/misc/ButtonPrimary";
 import ArowRight from "../../public/assets/Icon/ArowRight.svg";
+import LiveCpu from "../../public/assets/Icon/liveCPU.svg";
+import LiveCPUAtive from "../../public/assets/Icon/liveCPUAtive.svg";
+import Staking from "../../public/assets/Icon/staking.svg";
 import LogoVPN from "../../public/assets/logo-full.svg";
+// Import react scroll
+import { injected } from "@wagmi/connectors";
+import { connect, disconnect, getAccount } from "@wagmi/core";
+import { mainnet } from "@wagmi/core/chains";
+import { config } from "./../../config";
 
 const Layout = ({ children }) => {
   const asideLink = [
     {
       name: "Live GPU",
       url: "about",
+      icon: LiveCpu,
     },
     {
       name: "Leaderboard",
       url: "roadmap",
-    },
-    {
-      name: "Roadmap",
-      url: "gpu",
+      icon: LiveCPUAtive,
     },
     {
       name: "Staking",
       url: "stake",
+      icon: Staking,
     },
   ];
+  const account = getAccount(config);
+  console.log(account);
+  const login = async () => {
+    try {
+      const result = await connect(config, {
+        chainId: mainnet.id,
+        connector: injected(),
+      });
+    } catch (error) {
+      console.log("loi");
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await disconnect(config);
+    } catch (error) {
+      console.log("loi");
+    }
+  };
+
+  function shortenAddress(address) {
+    return `${address?.slice(0, 6)}…${address?.slice(-4)}`;
+  }
   return (
     <div class="antialiased bg-gray-50 dark:bg-gray-900">
-      <nav class="bg-white  px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
+      <nav class="bg-white  px-4 py-4 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
         <div class="flex flex-wrap justify-between items-center">
           <LogoVPN className="h-10 w-auto" />
-          <div class="flex items-center justify-end lg:order-2">
-            <ButtonPrimary addClass={""}>
-              <div className="flex justify-center items-center gap-2">
-                Connect Wallet <ArowRight />
-              </div>
-            </ButtonPrimary>
-          </div>
+          {account.isConnected ? (
+            <button
+              onClick={() => {
+                logout();
+              }}
+              className={
+                "font-medium flex items-center gap-2 text-[14px] tracking-wide py-[8px] px-4 sm:px-8 border border-seconday text-white-300  outline-none rounded-l-full rounded-r-full capitalize  hover:text-white-500 transition-all hover:shadow-seconday "
+              }
+            >
+              {shortenAddress(account?.address)} <ArowRight />
+            </button>
+          ) : (
+            <div class="flex items-center justify-end lg:order-2">
+              <ButtonPrimary onClick={login}>
+                <div className="flex justify-center items-center gap-2">
+                  Connect Wallet <ArowRight />
+                </div>
+              </ButtonPrimary>
+            </div>
+          )}
         </div>
       </nav>
 
@@ -45,24 +89,17 @@ const Layout = ({ children }) => {
           boxShadow: "0px 8px 28px 0px #0105114D",
         }}
       >
-        <div class="overflow-y-auto py-5 px-3 h-full bg-white dark:bg-gray-800">
-          <ul class="space-y-1">
+        <div class="overflow-y-auto py-5 px-4 h-full bg-white dark:bg-gray-800">
+          <ul class="space-y-2">
             {asideLink?.map((el, index) => (
               <li key={index}>
                 <a
                   href="#"
-                  class="flex border border-[#6A53FF66] h-10 rounded-lg items-center p-2 text-white-300 font-normal bg-[#0B0C14] text-[14px]  dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  class={`flex border border-[#6A53FF66] h-11 rounded-lg items-center p-2 px-4 text-white-300 font-normal bg-[#0B0C14] text-[14px]  dark:text-white hover:bg-gray-500 dark:hover:bg-gray-700 group ${
+                    index == 0 && "bg-[#2D285E] text-primary"
+                  }`}
                 >
-                  <svg
-                    aria-hidden="true"
-                    class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                    <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                  </svg>
+                  <el.icon />
                   <span class="ml-3">{el?.name} </span>
                 </a>
               </li>
